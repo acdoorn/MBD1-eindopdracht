8919d302ebabf214f984e4cb4c0711a390cd67b7
acdoorn <acdoorn@student.avans.nl>
2015-03-31 19:20:07 +0200

    end commit, everything works

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,3 @@
-// Open en sluit panel met swipe.
 $(document).on('pageinit', '#home', function(){
     $(document).on('swipeleft swiperight', '#home', function(e) {
         if(e.type === "swiperight") {
@@ -9,8 +8,7 @@ $(document).on('pageinit', '#home', function(){
     });
 });
 
-// Swipe up en down eventlisteners voor detail pagina.
-$(document).on('pageinit', '#detail', function(){
+$(document).on('pageinit', '#detail', function(){    
     if($('#name').text() == "") {
         showDetails(null, "slide");
     }
@@ -20,7 +18,6 @@ $(document).on('pageinit', '#detail', function(){
         if(id_next != "") {
             var rel = $('#'+id_next).attr('rel');
             var json = jQuery.parseJSON(rel);
-
             showDetails(json, "slideup");
         }
     });
@@ -29,41 +26,27 @@ $(document).on('pageinit', '#detail', function(){
         if(id_previous != "") {
             var rel = $('#'+id_previous).attr('rel');
             var json = jQuery.parseJSON(rel);
-
             showDetails(json, "slidedown");
         }
     });
 });
 
-// Wanneer de settings pagina geinitialiseerd wordt dan worden de opgeslagen waarden, tags en eventlisteners gezet.
 $(document).on('pageinit', '#settings', function(){
-    $('#tag').focusout(function() {
-        $('#autocompletelist').hide();
-    });
-
-    $('#tag').focusin(function() {
-        $('#autocompletelist').show();
-    });
-
     $('#save').on('tap', function(){
         save();
     });
     
-    setValues();
-    getTags();
+    setValues();//get from cache
 });
 
-// Wanneer de settings link wordt aangetapt, dan wordt de mobile.loading getoond.
 $(document).on('tap', '#settingslink', function(){
     startLoading();
 });
 
-// Wanneer de applicatie gestart wordt, dan wordt je huidige locatie opgehaald, indien dat gelukt is wordt de onSuccess functie uitgevoerd.
 function onDeviceReady(){
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }
 
-// Indien er settings in localStorage zijn opgeslagen, worden deze gezet in de settings pagina.
 function setValues() {
     var distance = window.localStorage.getItem("distance");
     if(distance > 0) {
@@ -73,11 +56,6 @@ function setValues() {
     var sortby = window.localStorage.getItem("sortby");
     if(sortby != null) {
         $('#sortby').val(sortby).selectmenu("refresh");
-    }
-
-    var tag = window.localStorage.getItem("tag");
-    if(tag != null) {
-        $('#tag-input').val(tag);
     }
 }
 
@@ -90,7 +68,6 @@ function onError(error) {
           'message: ' + error.message + '\n');
 }
 
-// Wanneer de savebutton aangeklikt wordt, wordt de volgende functie uitgevoerd.
 function save() {
     var refreshRestaurants = false;
     
@@ -106,43 +83,33 @@ function save() {
         refreshRestaurants = true;
     }
 
-    // var tag = $('#tag-input').val();
-    // if(tag !== window.localStorage.getItem("tag")){
-    //     window.localStorage.setItem("tag", tag);
-    //     refreshRestaurants = true;
-    // }
-
     if(refreshRestaurants === true){
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }
 }
 
-// getTags() haalt de tags op uit de API en cachet deze.
-function getTags(){
-    var url = "https://api.eet.nu/tags";
-    if(window.localStorage.getItem('cached_tags') == null) {
-        $.getJSON(url, function(data) {
-            var dataToStore = JSON.stringify(data);
-            window.localStorage.setItem('cached_tags', dataToStore);
-            $('#autocompletelist ul').empty();
-            $.each(data.results, function(i, line) {
-                $('#autocompletelist ul').append('<li onclick="fillInputField(\''+line.name+'\');"><a href="#">'+line.name+'</a></li>');
-                $('#autocompletelist ul').filterable("refresh");
-            });
-            $('#tag').focusout();
-        });
+function addToContacts() {
+    if($('#telephone').text() != "Unavailable") {
+        var myContact = navigator.contacts.create({"displayName": $('#name').text()});
+
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('HOME', $('#telephone').text(), false);
+        myContact.phoneNumbers = phoneNumbers;
+
+        myContact.save(onSuccessCallBack, onErrorCallBack);
+
+        function onSuccessCallBack(contact) {
+            alert($('#name').text() + " is added to your contacts!");
+        };
+
+        function onErrorCallBack(contactError) {
+            alert("Error = " + contactError.code);
+        };
     } else {
-        var data = JSON.parse(window.localStorage.getItem('cached_tags'));
-        $('#autocompletelist ul').empty();
-        $.each(data.results, function(i, line) {
-            $('#autocompletelist ul').append('<li onclick="fillInputField(\''+line.name+'\');"><a href="#">'+line.name+'</a></li>');
-            $('#autocompletelist ul').filterable("refresh");
-        });
-        $('#tag').focusout();
+        alert("No telephone found!");
     }
 }
 
-// getRestaurants haalt de restaurants uit de buurt op en genereerd de lijst met restaurants.
 function getRestaurants(position) {
     if(window.localStorage.getItem("distance") > 0){
         var distance = window.localStorage.getItem("distance");
@@ -156,17 +123,9 @@ function getRestaurants(position) {
         var sortby = $('#sortby').val();
     }
 
-    if(window.localStorage.getItem("tag") != null && window.localStorage.getItem("tag") != ""){
-        var tag = "&tags="+window.localStorage.getItem("tag");
-    } else if($('#tag-input').val() != null && $('#tag-input').val() != ""){
-        var tag = "&tags="+$('#tag-input').val();
-    } else {
-        var tag = "";
-    }
-
     var longitude = position.coords.longitude;
     var latitude = position.coords.latitude;
-    var url = "https://api.eet.nu/venues?max_distance="+distance+"&geolocation="+latitude+","+longitude+tag+"&sort_by="+sortby;
+    var url = "https://api.eet.nu/venues?max_distance="+distance+"&geolocation="+latitude+","+longitude+"&sort_by="+sortby;
     if(distance >= 0){
         startLoading();
         $.getJSON(url, function(data) {
@@ -190,7 +149,11 @@ function getRestaurants(position) {
                 jsonObject.next = next;
                 jsonObject.previous = previous;
                 var relString = JSON.stringify(jsonObject);
-                var rating = (line.rating !== null ? '<span style="float:right">'+line.rating+'</span>' : '')
+                var rating = '';
+                if(line.rating !== null) {
+                    rating = line.rating.toString();
+                    rating = '<span style="float:right">'+rating.charAt(0)+','+rating.charAt(1)+'</span>';
+                }
                 $('#restaurantlist').append('<li id="'+line.id+'" rel'+"='"+relString+"'"+'><a href="#" data-transition="flip" class="ui-btn ui-corner-all ui-shadow ui-btn-inline">'+line.name+rating+'</a></li>');
             });    
             stopLoading();
@@ -207,9 +170,7 @@ function getRestaurants(position) {
     }
 }
 
-// Indien er een nieuwe detail pagina geopend wordt dan wordt deze functie uitgevoerd.
 function showDetails(json, slide_direction) {
-
     if(json != null) {
         window.localStorage.removeItem("lastDetails");
         window.localStorage.setItem("lastDetails", JSON.stringify(json));
@@ -222,10 +183,61 @@ function showDetails(json, slide_direction) {
     startLoading();
     $.getJSON(url, function(data) {
 
+        var lat = data.geolocation.latitude;
+        var lng = data.geolocation.longitude;
+
         // Title and Navigation
         $('#name').text(data.name);
         $('#id_next').val(json.next);
         $('#id_previous').val(json.previous);
+
+                // Beoordeling
+        var urlReview = "https://api.eet.nu/venues/" + id + "/reviews";
+        $.getJSON(urlReview, function(reviewData) {
+            if(reviewData.results.length == 0) {
+                $('#ratings').hide();
+            } else {
+                var rating = '';
+                if(data.rating !== null) {
+                    rating = data.rating.toString();
+                    rating = rating.charAt(0)+','+rating.charAt(1);
+                    $('#eetnu').text(rating);
+                    $('#eetnuRating').show();
+                } else {
+                    $('#eetnuRating').hide();
+                }
+                var reviews = 0;
+                var ratings = {"rating": 0, "ambiance": 0, "food": 0, "service": 0, "value": 0};
+                $.each(reviewData.results, function(i, j) {
+                    if(j.rating != null) {
+                        reviews++;
+                        ratings["rating"] = ratings["rating"] + j.scores.ambiance;;
+                        ratings["ambiance"] = ratings["ambiance"] + j.scores.ambiance;
+                        ratings["food"] = ratings["food"] + j.scores.food;
+                        ratings["service"] = ratings["service"] + j.scores.service;
+                        ratings["value"] = ratings["value"] + j.scores.value;
+                    }
+                });
+
+                $.each(ratings, function(i, j) {
+                    ratings[i] = Math.round(j / reviews);
+                    ratings[i] = ratings[i].toString();
+                    if(ratings[i] != "NaN") {
+                        ratings[i] = ratings[i].substr(0, 1) + ratings[i].substr(1);
+                    } else {
+                        ratings[i] = 0;
+                    }
+                    if(ratings[i] == 100) {
+                        $('#' + i).text(ratings[i].charAt(0) + ratings[i].charAt(1));
+                    }
+                    else {
+
+                        $('#' + i).text(ratings[i].charAt(0) +','+ ratings[i].charAt(1));
+                    }
+                });
+                $('#ratings').show();
+            }
+        });
 
         // Map
         $('#map_canvas').gmap({ 'center': new google.maps.LatLng(data.geolocation.latitude,data.geolocation.longitude), 
@@ -249,57 +261,13 @@ function showDetails(json, slide_direction) {
         $('#telephone').attr("onclick", "window.open('tel:" + data.telephone + "', '_system');");
         if(data.website_url != null && data.website_url != "") {
             $('#own_url').attr("onclick", "navigator.app.loadUrl('" + data.website_url + "', { openExternal:true });");
-            $('#own_url').text("Restaurant");
+            $('#own_url').text(data.name);
             $('#restaurant_url').show();
         } else {
             $('#own_url').attr("onclick", "");
-            $('#own_url').text("Restaurant");
             $('#restaurant_url').hide();
         }
         $('#eetnu_url').attr("onclick", "navigator.app.loadUrl('" + data.url + "', { openExternal:true });");
-
-        // Beoordeling
-        var urlReview = "https://api.eet.nu/venues/" + id + "/reviews";
-        $.getJSON(urlReview, function(reviewData) {
-            if(reviewData.results.length == 0) {
-                $('#ratings').hide();
-            } else {
-                var nOfReview = 0;
-                var ratings = {"rating": 0, "ambiance": 0, "food": 0, "service": 0, "value": 0};
-                $.each(reviewData.results, function(k, v) { // k = key, v = value.
-                    if(v.rating != null) {
-                        nOfReview++;
-                        ratings["rating"] = ratings["rating"] + v.rating;
-                        ratings["ambiance"] = ratings["ambiance"] + v.scores.ambiance;
-                        ratings["food"] = ratings["food"] + v.scores.food;
-                        ratings["service"] = ratings["service"] + v.scores.service;
-                        ratings["value"] = ratings["value"] + v.scores.value;
-                    }
-                });
-
-                var maxScore = 10 * nOfReview;
-                $.each(ratings, function(k, v) { // k = key, v = value.
-                    ratings[k] = Math.round(v / nOfReview);
-                    ratings[k] = ratings[k].toString();
-                    if(ratings[k] == "100") {                
-                        $('#' + k + "_stars").html(getRating(ratings[k].charAt(0) + "" + ratings[k].charAt(1)));
-                    } else {
-                        $('#' + k + "_stars").html(getRating(ratings[k].charAt(0)));
-                    }
-                    if(ratings[k] != "NaN") {
-                        if(ratings[k] == "100") {
-                            ratings[k] = ratings[k].slice(0,-1);
-                        } else {
-                            ratings[k] = ratings[k].substr(0, 1) + "," + ratings[k].substr(1);
-                        }
-                    } else {
-                        ratings[k] = 0;
-                    }
-                    $('#' + k).text(ratings[k]);
-                });
-                $('#ratings').show();
-            }
-        });
 
         // Openingstijden
         if(data.opening_hours.length == 0) {
@@ -331,41 +299,13 @@ function showDetails(json, slide_direction) {
             $('#descriptionfield').show();
         }
 
-        // Menukaarten
-        if(data.tags.length == 0) {
-            $('#tags').hide();
-        } else {
-            for (var i = 0; i < 7; i++) {
-                $('#day_' + i).text("");
-            }
-            $.each(data.opening_hours, function(k, v) { // k = key, v = value.
-                if(v.closed == true) {
-                    $('#day_' + v.day).text("Closed");
-                } else {
-                    var start = "", end = "";
-                    if(v.lunch_from == null) { start = v.dinner_from; } else { start = v.lunch_from; }
-                    if(v.dinner_till == null){ end = v.lunch_till;    } else { end = v.dinner_till;  }
-                    if(start == null) { start = "?"  }
-                    if(end == null)   { end = "?"    }
-                    $('#day_' + v.day).text(start + " - " + end);
-                }
-            });
-            $('#tags').show();
-        }
+
 
         $.mobile.changePage("#detail", { transition: slide_direction });
         stopLoading();
     });
 }
 
-// fillInputField zorgt ervoor dat indien er op een tag geklikt is dat deze waarde in de textbox komt en de suggestie lijst verdwijnt.
-function fillInputField(name) {
-    $('#tag-input').val(name);
-    $('#tag-input').change();
-    $('#tag').focusout();
-}
-
-// Toont het loading schermpje
 function startLoading() {
     var $this = $( this ),
         theme = "a",
@@ -381,48 +321,7 @@ function startLoading() {
     });
 }
 
-// Stopt het loading scherm. 
 function stopLoading() {
     $.mobile.loading( "hide" );
-}
-
-function getRating(rating) {
-    var returnString = "";
-    switch (rating) {
-        case "1": returnString = "<i class=\"fa fa-star-half-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i>"; break;
-        case "2": returnString = "<i class=\"fa fa-star\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i>"; break;
-        case "3": returnString = "<i class=\"fa fa-star\"></i><i class=\"fa fa-star-half-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i>"; break;
-        case "4": returnString = "<i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i>"; break;
-        case "5": returnString = "<i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star-half-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i>"; break;
-        case "6": returnString = "<i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i>"; break;
-        case "7": returnString = "<i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star-half-o\"></i><i class=\"fa fa-star-o\"></i>"; break;
-        case "8": returnString = "<i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star-o\"></i>"; break;
-        case "9": returnString = "<i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star-half-o\"></i>"; break;
-        case "10": returnString = "<i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i><i class=\"fa fa-star\"></i>"; break;
-        default: returnString = "<i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i><i class=\"fa fa-star-o\"></i>"; break;
-    }
-    return returnString;
-}
-
-function addToContacts() {
-    if($('#telephone').text() != "Unavailable") {
-        var myContact = navigator.contacts.create({"displayName": $('#name').text()});
-
-        var phoneNumbers = [];
-        phoneNumbers[0] = new ContactField('HOME', $('#telephone').text(), false);
-        myContact.phoneNumbers = phoneNumbers;
-
-        myContact.save(onSuccessCallBack, onErrorCallBack);
-
-        function onSuccessCallBack(contact) {
-            alert($('#name').text() + " is added to your contacts!");
-        };
-
-        function onErrorCallBack(contactError) {
-            alert("Error = " + contactError.code);
-        };
-    } else {
-        alert("No telephone found!");
-    }
 }
 
